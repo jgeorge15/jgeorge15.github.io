9bc1ac0e1e5ab5c5cdad489cc373082ded990d80
jgeorge15 <jgeorge15@users.noreply.github.com>
2018-10-03 13:00:45 -0400

    new

---
+++ Assignment3/hw3.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>PHP Functions and Arrays</title>
+	<meta name="description" content="Fuctions"></meta>
+	<meta name="keywords" content="HTML"></meta>
+	<meta name="author" content="Jaibu George"></meta>
+	<link rel="stylesheet" href="hw3.css"></link>
+</head>
+<body>
+	<?php
+	function isBitten() {
+		$check = rand([0,1]);
+	}
+	if $check == 1 {
+		echo 'Charlie ate my lunch!';
+	else 
+		echo 'Charlie did not eat my lunch';
+	}
+	?>
+</body>
+</html>
